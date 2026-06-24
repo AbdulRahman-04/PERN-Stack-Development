eb29e4b2e872a514b5fa05a5ec519c77c3d8f5f3
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-06-25 02:39:34 +0530

    Js Operators

---
+++ 02-JavaScript/01-basics/2.js
@@ -0,0 +1,67 @@
+// OPERATORS IN JAVASCRIPT : there are several type of operators in js 
+
+// Arithmetic operatos : + , - , / , % , * , ** used for mathetmatical operations 
+
+// let a = 12
+// let b = 13
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a*b);
+// console.log(a**b);
+
+
+// Assignment operators  : this operator is used to assign values to variables
+//  = , += , -=, /=, %=, *=
+
+let myName = "rahman"
+let num1 = 30
+let num2 = 15
+// console.log(num1+=num2);
+// console.log(num1-=num2);
+// console.log(num1/=num2);
+// console.log(num1%=num2);
+
+
+// // Comparison Operators : this operator is used to compare values & data types
+//  == , != , === , !==, >, <, >=, <=
+
+let n1 = 45
+let n2 = 48
+// console.log(n1==n2);
+// console.log(n1!= n2);
+// console.log(n1===n2);
+// console.log(n1!==n2);
+// console.log(n1>n2, n1<2, n1>=n2, n1<= n2);
+
+
+
+// Logical Operators : && (true if both condition true) , || (true if any one is true) , ! (gives false condition true and true condition false)
+let a = 20
+let b = 20
+
+// console.log(a == b && a === b);
+// console.log(a==b || a!==b);
+// console.log(!(a==b));
+
+
+// Bitwise operators : & , | , !, ~
+
+// console.log(5 & 3);
+// console.log(5 | 3);
+// console.log(5 ^ 3);
+// console.log(~5);
+
+
+// ternary operators : this operator is used for single line conditions.
+let myAge = 17
+
+// if (myAge >= 18) {
+//     console.log('You can drive nigga');
+// }
+
+// string operator : + used in implicit conversion to concatinate other datatype values into string
+// let value = "54" + 54
+// console.log(value, typeof(value));
