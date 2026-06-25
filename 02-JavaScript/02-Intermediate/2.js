d8b87a11be2adc89f7108ec2ef1def967725863a
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-06-26 02:52:15 +0530

    Loops in JS

---
+++ 02-JavaScript/02-Intermediate/2.js
@@ -0,0 +1,92 @@
+// Loops: there three typess of loops in js : for loop, while loop, do while loop
+
+// for loop : For loop is an entry-controlled loop used when the number of iterations is known.
+// for(i=0;i<=10;i++){
+//     // console.log(i);
+// }
+
+// Q1 Print numbers from 1 to 20
+// for(i=1;i<=20;i++){
+//     console.log(i);
+
+// }
+
+// Q2 Print even numbers from 2 to 20
+// for(i=0;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// Print the multiplication table of 9 using a for loop.
+// for(n=9;n<=90;n++){
+//     if(n%9==0){
+//         console.log(n);
+//     }
+// }
+
+// Print all numbers from 1 to 50 that are divisible by 3 and 5.
+// for(n=1;n<=50;n++){
+//     if(n%3==0 && n%5==0){
+//         console.log(n);
+//     }
+// }
+
+// // Find the sum of all even numbers from 1 to 100.
+// let sum = 0
+// for(i=0;i<=100;i++){
+//     if(i%2==0){
+//         sum +=i;
+//     }
+// }
+
+// console.log(sum);
+
+
+// while loop : While loop is an entry-controlled loop that executes as long as the condition remains true.
+// while(j<=20){
+//     // console.log(j);
+//     j++
+// }
+
+// Q1 Print numbers from 10 to 1
+// let i = 10;
+// while (i >= 1) {
+//   console.log(i);
+//   --i;
+// }
+
+// Q2 Print multiples of 5 till 50
+// let i = 5;
+// while (i <= 50) {
+//   if (i % 5 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// this is an exit controlled loop, where conditioned is checked after executing the code block.
+// let rollNo = 49;
+
+// do {
+//   // console.log(rollNo);
+//   rollNo++;
+// } while (rollNo <= 55);
+
+// Q1 Print numbers from 1 to 5
+// let num = 1;
+// do {
+//     console.log(num);
+//     num++
+    
+// }while(num<=5)
+
+// // Q2 Print odd numbers from 1 to 15
+// let num = 1;
+// do{
+//     if(num%2!==0){
+//         console.log(num);
+//     }
+//     num++
+    
+// }while(num<=15)
