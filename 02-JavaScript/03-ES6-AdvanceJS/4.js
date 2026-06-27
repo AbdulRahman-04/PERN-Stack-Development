b4bafe92182610c6260eb8e0c040a2d214473bef
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-06-28 04:06:42 +0530

    fetch nd async and await in js

---
+++ 02-JavaScript/03-ES6-AdvanceJS/4.js
@@ -0,0 +1,51 @@
+// async and await
+
+// Async/Await is the easiest and most modern way
+// to handle asynchronous JavaScript.
+
+// It does NOT replace Promises.
+// It is a cleaner syntax built on top of Promises.
+
+// fetch() immediately returns a pending Promise.
+
+// await waits until that Promise resolves
+// and gives us a Response object.
+
+// response.json() reads the response body
+// and parses the JSON.
+
+// Since parsing is also asynchronous,
+// response.json() returns another Promise.
+
+// Therefore, we use another await
+// to get the final JavaScript object.
+
+async function getData() {
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+    const data = await response.json();
+
+    console.log(data);
+
+}
+
+// getData();
+
+
+function getPromiseData(){
+ 
+    return fetch("https://jsonplaceholder.typicode.com/todos/1").then((response)=>{
+        return response.json();
+    })
+
+}
+
+function main(){
+    getPromiseData().then((data)=>{
+        console.log(data);
+        
+    })
+}
+
+main()
